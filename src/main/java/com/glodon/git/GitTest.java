e39a25eb24c0347822c32c0737d384bbf7a4fa63
xiapw <xiapw@qq.com>
2023-01-02 11:10:32 +0800

    2222

--- src/main/java/com/glodon/git/GitTest.java
+++ src/main/java/com/glodon/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hah");
         System.out.println("");
+        System.out.println("weee");
     }
 }
