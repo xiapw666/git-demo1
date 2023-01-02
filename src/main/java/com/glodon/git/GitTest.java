321eb8b5bf0e8afc59411bbd12e0098c0aed584e
xiapw <xiapw@qq.com>
2023-01-02 17:43:03 +0800

    hot-2-commit34

--- src/main/java/com/glodon/git/GitTest.java
+++ src/main/java/com/glodon/git/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("hot-fix");
         System.out.println("hot-fix1");
         System.out.println("hot-fix2");
+        System.out.println("hot-fix3");
     }
 }
