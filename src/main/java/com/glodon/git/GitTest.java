d57d2a7e58905575594daf44955b7805c560df6a
xiapw <xiapw@qq.com>
2023-01-02 11:09:30 +0800

    hhhh

---
+++ src/main/java/com/glodon/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.glodon.git;
+
+/**
+ * @author shkstart
+ * @create 2023-01-02 11:03
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hah");
+        System.out.println("");
+    }
+}
